dc941c90cae7aaad08aa446c0bdfe4456678f1f9
afillimonov <146652443+afillimonov@users.noreply.github.com>
2025-03-18 16:22:06 +0300

    Rework interaction with breakable blocks

--- src/main/java/com/afillimonov/fallingfix/FallingFix.java
+++ src/main/java/com/afillimonov/fallingfix/FallingFix.java
@@ -4,11 +4,13 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FallingFix extends JavaPlugin implements Listener {
@@ -20,25 +22,36 @@ public class FallingFix extends JavaPlugin implements Listener {
     @EventHandler
     public void onFallBlock(EntityChangeBlockEvent e) {
         Entity entity = e.getEntity();
+
         if (entity.getType() == EntityType.FALLING_BLOCK && e.getTo() == Material.AIR) {
             World world = e.getBlock().getWorld();
             double x = e.getBlock().getLocation().getX();
             double y = e.getBlock().getLocation().getY();
             double z = e.getBlock().getLocation().getZ();
-            Material block = e.getBlock().getType();
+            Material fallingBlockMaterial = e.getBlock().getType();
+
             e.getBlock().setType(Material.AIR);
 
             while (y > 0.0D) {
                 Location loc = new Location(world, x, y, z);
-                if (loc.getBlock().getType() != Material.AIR && loc.getBlock().getType() != Material.WATER && loc.getBlock().getType() != Material.LAVA) {
-                    loc.setY(y + 1.0D);
-                    loc.getBlock().setType(block);
+                Block blockBelow = loc.getBlock();
+
+                if (blockBelow.getType() == Material.AIR || blockBelow.isLiquid()) {
+                    y--;
+                    continue;
+                }
+                if (isBreakable(blockBelow)) {
+                    blockBelow.setType(fallingBlockMaterial);
                     break;
                 }
-                y--;
+                loc.setY(y + 1.0D);
+                loc.getBlock().setType(fallingBlockMaterial);
+                break;
             }
-
             e.setCancelled(true);
         }
+    }
+    private boolean isBreakable(Block block) {
+        return block.isPassable() || block.isEmpty() || block.isLiquid();
     }
 }
